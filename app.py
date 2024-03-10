ce87bb8cf94d0aa3626e404cf7ad78fc40df941a
GlebiN <92937304+GlebiN@users.noreply.github.com>
2024-03-10 11:55:16 +0200

    Add limit switch support

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import time
 from servo import Servo
-import machine
+from picozero import Button
 
+BUTTON_PORT = 5
 SERVO_PORT = 16
 LIMIT_SWITCH_PORT = 22
 
-PULL_UP = False
-DELAY = 0.5
-END = 90
-START = 0
+NORMALY_OPEN = False
+DELAY = 0.5 #seconds
+START = 20
+END = START+90
+
 
 servo = Servo(pin_id=SERVO_PORT)
 
-limit_switch = machine.Pin(LIMIT_SWITCH_PORT, machine.Pin.IN, pull=PULL_UP)
+limit_switch = Button(BUTTON_PORT)
 
 def rotate(position: int) -> None:
     servo.write(position)
@@ -26,15 +28,25 @@ def run_servo_routine() -> None:
     rotate(END)
     sleep(DELAY)
 
-def is_limit_switch_pressed() -> bool:
-    if PULL_UP:
-        return limit_switch.value() == 0
+def toggle_servo() -> None:
+    if (place == END):
+        rotate(START)
+        place = START
     else:
-        return limit_switch.value() == 1
+        rotate(END)
+        place = END
 
+def rotate_to_end() -> None:
+    rotate(END)
+
+def rotate_to_start() -> None:
+    rotate(START)
+    
 if __name__ == '__main__':
     while True:
-        if is_limit_switch_pressed():
-            run_servo_routine()
-            
-        print(f'{is_limit_switch_pressed()=}')
+        limit_switch.when_released = rotate_to_start
+        limit_switch.when_pressed = rotate_to_end
+
+        sleep(0.2)
+        print(f'{limit_switch.value}')
+
